bf43683d9a093200e4b226371a1d5c4912b5fd6c
Fidelito Pardillo <63651138+fidsolit@users.noreply.github.com>
2026-09-16 19:13:57 +0800

    modified nexconfigts

--- webposv3/next.config.ts
+++ webposv3/next.config.ts
@@ -1,8 +1,16 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   // your other existing configs here
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  // your other existing configs here
+  /* remove output: "export" */
 };
 
 export default nextConfig;
